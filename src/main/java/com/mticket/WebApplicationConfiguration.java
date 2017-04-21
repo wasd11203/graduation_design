e534142c1600c90e38f62c81ac12ee4354a1ffd0
wasd11203 <383684855@qq.com>
2017-04-21 20:45:13 +0800

    修改打包方式 == 完成由jar 转换到 war包

--- src/main/java/com/mticket/WebApplicationConfiguration.java
+++ src/main/java/com/mticket/WebApplicationConfiguration.java
@@ -2,7 +2,9 @@ package com.mticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Spring Boot 启动类
@@ -11,8 +13,16 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
  */
 @SpringBootApplication
 @ServletComponentScan // 使得spring能够扫描到我们自己编写的servlet和filter(针对druid) === 针对的是 启动druid 的监控
-public class WebApplicationConfiguration {
+public class WebApplicationConfiguration extends SpringBootServletInitializer{
 	
+	/**
+	 * war 包启动入口
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WebApplicationConfiguration.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplicationConfiguration.class, args);
 	}
